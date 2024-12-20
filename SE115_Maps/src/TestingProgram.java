f16ae49fec8f911019821f8645b570ca2a264163
Kerim Karakan <karakankerim@gmail.com>
2024-12-20 12:43:02 +0300

    One more thing
    I realized there was nothing happening when there are no arguments or more than 2 arguments given from the command line. So, I handled that error which would be an "ArrayIndexOutOfBoundsException".

--- SE115_Maps/src/TestingProgram.java
+++ SE115_Maps/src/TestingProgram.java
@@ -1,6 +1,11 @@
 public class TestingProgram {
 
     public static void main(String[] args) {
+
+        if(args.length != 2){
+            System.out.println("There are no arguments given or more than 2 arguments.\nPlease give exactly 2 arguments to run the program.");
+            return;
+        }
         String inputFile = args[0];
         String outputFile = args[1];
         WayFinder wayFinder = new WayFinder();
